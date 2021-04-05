1c8ab42de9e200b9c492149d52d800c152d9178e
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-05 12:45:16 +0200

    sync files

--- src/main/java/cz/cvut/fel/pjv/Figurines/Figurine.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Figurine.java
@@ -7,5 +7,17 @@ public class Figurine {
     private char color;
     private int type;
 
+    public Figurine(char color, int type) {
+        this.color = color;
+        this.type = type;
+    }
+
+    /**
+     * @return possible moves by type of figure as a 2D array.
+     */
+    public int[][] getPossibleMoves(){
+        return null;
+    }
+
 }
 
