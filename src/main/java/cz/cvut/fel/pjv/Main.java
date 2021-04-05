601f422081518477d735b2a1115cd6411cf3b2b5
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-05 10:01:47 +0200

    print hello world

--- src/main/java/cz/cvut/fel/pjv/Main.java
+++ src/main/java/cz/cvut/fel/pjv/Main.java
@@ -1,7 +1,7 @@
 package cz.cvut.fel.pjv;
 
 public class Main {
-    static public void Main(String []args) {
+    static public void main(String []args) {
         System.out.println("Hello world!");
     }
 }
