423653dee1e3b2dbf2ee35d595cb63a3ac1da167
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-05 10:34:49 +0200

    add Chessboard constructor with a few comments

--- src/main/java/cz/cvut/fel/pjv/Chessboard.java
+++ src/main/java/cz/cvut/fel/pjv/Chessboard.java
@@ -1,4 +1,30 @@
 package cz.cvut.fel.pjv;
 
+
+/**
+ * Class representations Chessboard as a 2D array.
+ */
 public class Chessboard {
+    private int [][]positions;
+    private int gameRound;
+    private char playerColor;
+
+    private int countOfBlackFigurines;
+    private int countOfWhiteFigurines;
+
+    /**
+     * @param positions representations Chessboard as a 2D array. Load positions from a data file by type of game.
+     * @param gameRound
+     * @param playerColor determines which player is on the move (black player - B, white color - W).
+     * @param countOfBlackFigurines
+     * @param countOfWhiteFigurines
+     */
+    public Chessboard(int[][] positions, int gameRound, char playerColor, int countOfBlackFigurines, int countOfWhiteFigurines) {
+        this.positions = positions;
+        this.gameRound = gameRound;
+        this.playerColor = playerColor;
+        this.countOfBlackFigurines = countOfBlackFigurines;
+        this.countOfWhiteFigurines = countOfWhiteFigurines;
+    }
+
 }
