7cc7f9d322b0e8cf7ad882f5367f9d853335c1ad
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-06 15:30:32 +0200

    add methods for check possibles moves

--- src/main/java/cz/cvut/fel/pjv/Figurines/Figurine.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Figurine.java
@@ -1,5 +1,7 @@
 package cz.cvut.fel.pjv.Figurines;
 
+import cz.cvut.fel.pjv.Chessboard;
+
 /**
  * Class for manipulations with Figurines.
  */
@@ -17,6 +19,29 @@ public class Figurine {
      */
     public int[][] getPossibleMoves(){
         return null;
+    }
+
+
+    /**
+     * @param x coordinates of the position
+     * @param y coordinates of the position
+     * @param chessboard info of board
+     * @return true - if the move is possible or false - if it isn't possible
+     */
+    public boolean pinedPeace(int x, int y, Chessboard chessboard){
+        return false;
+    }
+
+    /**
+     * @param x coordinates of the position
+     * @param y coordinates of the position
+     * @param chessboard1 info of board
+     * @param chessboard2 info of board
+     * @param chessboard3 info of board
+     * @return true - if the move is possible or false - if it isn't possible
+     */
+    public boolean threefoldRepetition(int x, int y, Chessboard chessboard1, Chessboard chessboard2, Chessboard chessboard3){
+        return false;
     }
 
 }
