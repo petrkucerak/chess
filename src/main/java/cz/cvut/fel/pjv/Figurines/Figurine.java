66e98ae71ef695b5cf2258a08ad7f2ee56d9bbc8
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-06 15:55:04 +0200

    repair broken syntax

--- src/main/java/cz/cvut/fel/pjv/Figurines/Figurine.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Figurine.java
@@ -4,31 +4,41 @@ import cz.cvut.fel.pjv.Chessboard;
 
 /**
  * Class for manipulations with Figurines.
- * Type of figurines are represented as numbers by this key:
- * • king - 9
- * • queen - 8
- * • rook - 7
- * • bishop - 6
- * • knight - 5
- * • pawn - 4
- * Color of figurines are represented as decimal multiple by this key:
- * • black - 10
- * • white - 20
- * (For example, the black king has a number 19 or the white pawn has the number 24)
  */
 public class Figurine {
     private char color;
     private int type;
+    private int codeName;
 
+    /**
+     * @param color B - if figurine is black, W - if figurine is white
+     * @param type king - 9, queen - 8, rook - 7, bishop - 6, knight - 5, pawn - 4
+     * @variable codeName:
+     * Type of figurines are represented as numbers by this key:
+     * • king - 9
+     * • queen - 8
+     * • rook - 7
+     * • bishop - 6
+     * • knight - 5
+     * • pawn - 4
+     * Color of figurines are represented as decimal multiple by this key:
+     * • black - 10
+     * • white - 20
+     * (For example, the black king has a number 19 or the white pawn has the number 24)
+     */
     public Figurine(char color, int type) {
         this.color = color;
         this.type = type;
+        int tmp = 0;
+        if(this.color == 'B') tmp = 10;
+        if(this.color == 'W') tmp = 20;
+        this.codeName = tmp + type;
     }
 
     /**
      * @return possible moves by type of figure as a 2D array.
      */
-    public int[][] getPossibleMoves(){
+    public int[][] getPossibleMoves() {
         return null;
     }
 
