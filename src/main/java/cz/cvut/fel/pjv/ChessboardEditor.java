906307ff080d871d2c8c6852985a1a40509c8225
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-06 15:35:14 +0200

    add methods for a exporting custom chessboard

--- src/main/java/cz/cvut/fel/pjv/ChessboardEditor.java
+++ src/main/java/cz/cvut/fel/pjv/ChessboardEditor.java
@@ -5,4 +5,11 @@ package cz.cvut.fel.pjv;
  * Class for a creating custom position on the chessboard.
  */
 public class ChessboardEditor {
+
+    /**
+     * export Chessboard as a text file
+     */
+    public void exportChessboard(){
+
+    }
 }
