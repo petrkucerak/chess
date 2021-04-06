3939c88afe5ef2991fc85696e9357621f50c0a77
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-06 16:09:46 +0200

    add castles into king class

--- src/main/java/cz/cvut/fel/pjv/Figurines/King.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/King.java
@@ -1,8 +1,38 @@
 package cz.cvut.fel.pjv.Figurines;
 
+import cz.cvut.fel.pjv.Chessboard;
+
+/**
+ * Class for the king figurine
+ */
 public class King extends Figurine {
 
     public King(char color) {
         super(color, 9);
+    }
+
+    @Override
+    public int[][] getPossibleMoves() {
+        return super.getPossibleMoves();
+    }
+
+    /**
+     * @param x coordinates of the position
+     * @param y coordinates of the position
+     * @param chessboard info of board
+     * @return true - if the move is possible or false - if it isn't possible
+     */
+    private boolean queenSideCastle(int x, int y, Chessboard chessboard){
+        return false;
+    }
+
+    /**
+     * @param x coordinates of the position
+     * @param y coordinates of the position
+     * @param chessboard info of board
+     * @return true - if the move is possible or false - if it isn't possible
+     */
+    private boolean  kingSideCastle(int x, int y, Chessboard chessboard){
+        return false;
     }
 }
