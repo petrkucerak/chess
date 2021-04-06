7f27527c33138060327c0701c799f98e764257c5
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-04-06 16:11:35 +0200

    add javaDoc

--- src/main/java/cz/cvut/fel/pjv/Figurines/Queen.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Queen.java
@@ -1,7 +1,15 @@
 package cz.cvut.fel.pjv.Figurines;
 
+/**
+ * Class for the king figurine
+ */
 public class Queen extends Figurine {
     public Queen(char color) {
         super(color, 8);
+    }
+
+    @Override
+    public int[][] getPossibleMoves() {
+        return super.getPossibleMoves();
     }
 }
