7fdb302588f650e8db99860f50c7f2f7656b0917
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-07 21:09:52 +0200

    Bishop possible function

--- src/main/java/cz/cvut/fel/pjv/Figurines/Bishop.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Bishop.java
@@ -15,6 +15,36 @@ public class Bishop extends Figurine {
         // create new board
         boolean[][] possibleMoves = new boolean[8][8];
         // set all position as empty
+        setAllPositionFalse(possibleMoves);
+        // set positive moves
+        int tmp_y = y + 1;
+        int tmp_x = x + 1;
+        while(tmp_x < possibleMoves.length && tmp_y < possibleMoves.length){
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x ++;
+            tmp_y ++;
+        }
+        tmp_y = y - 1;
+        tmp_x = x + 1;
+        while(tmp_x < possibleMoves.length && tmp_y >= 0){
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x ++;
+            tmp_y --;
+        }
+        tmp_y = y - 1;
+        tmp_x = x - 1;
+        while(tmp_x >= 0 && tmp_y >= 0){
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x --;
+            tmp_y --;
+        }
+        tmp_y = y + 1;
+        tmp_x = x - 1;
+        while(tmp_x >= 0 && tmp_y < possibleMoves.length){
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x --;
+            tmp_y ++;
+        }
         return possibleMoves;
     }
 }
