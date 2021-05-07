482a6f2cc006c940c93caf639507e55e4df2b0bb
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-07 21:24:35 +0200

    Rook possibleMoves function

--- src/main/java/cz/cvut/fel/pjv/Figurines/Rook.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Rook.java
@@ -18,6 +18,27 @@ public class Rook extends Figurine {
         boolean[][] possibleMoves = new boolean[8][8];
         // set all position as empty
         setAllPositionFalse(possibleMoves);
+        // set positive moves
+        int tmp_y = y + 1;
+        while (tmp_y < possibleMoves.length) {
+            possibleMoves[tmp_y][x] = true;
+            tmp_y++;
+        }
+        tmp_y = y - 1;
+        while (tmp_y >= 0) {
+            possibleMoves[tmp_y][x] = true;
+            tmp_y--;
+        }
+        int tmp_x = x + 1;
+        while (tmp_x < possibleMoves.length) {
+            possibleMoves[y][tmp_x] = true;
+            tmp_x++;
+        }
+        tmp_x = x - 1;
+        while (tmp_x >= 0) {
+            possibleMoves[y][tmp_x] = true;
+            tmp_x--;
+        }
         return possibleMoves;
     }
 }
