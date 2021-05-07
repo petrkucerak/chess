d90f33e711f52b16027429f02689bc8257d3aa2b
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-07 22:06:47 +0200

    King possibleMove function

--- src/main/java/cz/cvut/fel/pjv/Figurines/King.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/King.java
@@ -17,6 +17,15 @@ public class King extends Figurine {
         boolean[][] possibleMoves = new boolean[8][8];
         // set all position as empty
         setAllPositionFalse(possibleMoves);
+        // set possible moves
+        if (x != 0) possibleMoves[y][x - 1] = true;
+        if (y != 0) possibleMoves[y - 1][x] = true;
+        if (x != possibleMoves.length - 1) possibleMoves[y][x + 1] = true;
+        if (y != possibleMoves.length - 1) possibleMoves[y + 1][x] = true;
+        if (x != 0 && y != 0) possibleMoves[y - 1][x - 1] = true;
+        if (x != 0 && y != possibleMoves.length - 1) possibleMoves[y + 1][x - 1] = true;
+        if (x != possibleMoves.length - 1 && y != 0) possibleMoves[y - 1][x + 1] = true;
+        if (x != possibleMoves.length - 1 && y != possibleMoves.length - 1) possibleMoves[y + 1][x + 1] = true;
         return possibleMoves;
     }
 }
