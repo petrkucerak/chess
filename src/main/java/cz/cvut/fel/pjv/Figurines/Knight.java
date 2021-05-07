b8c931144a5a42c699b4a3dc7ef7f00320a850ac
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-07 22:22:28 +0200

    Knight possibleMove function

--- src/main/java/cz/cvut/fel/pjv/Figurines/Knight.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Knight.java
@@ -15,6 +15,15 @@ public class Knight extends Figurine {
         boolean[][] possibleMoves = new boolean[8][8];
         // set all position as empty
         setAllPositionFalse(possibleMoves);
+        // set possible moves
+        if (isValidPosition(x - 2, y - 1)) possibleMoves[y - 1][x - 2] = true;
+        if (isValidPosition(x - 2, y + 1)) possibleMoves[y + 1][x - 2] = true;
+        if (isValidPosition(x + 2, y - 1)) possibleMoves[y - 1][x + 2] = true;
+        if (isValidPosition(x + 2, y + 1)) possibleMoves[y + 1][x + 2] = true;
+        if (isValidPosition(x - 1, y - 2)) possibleMoves[y - 2][x - 1] = true;
+        if (isValidPosition(x + 1, y - 2)) possibleMoves[y - 2][x + 1] = true;
+        if (isValidPosition(x - 1, y + 2)) possibleMoves[y + 2][x - 1] = true;
+        if (isValidPosition(x + 1, y + 2)) possibleMoves[y + 2][x + 1] = true;
         return possibleMoves;
     }
 }
