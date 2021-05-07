4baac35f7d5b5ce368b9e6b290cca8badc71c5e6
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-07 21:27:18 +0200

    Queen possibleMoves function

--- src/main/java/cz/cvut/fel/pjv/Figurines/Queen.java
+++ src/main/java/cz/cvut/fel/pjv/Figurines/Queen.java
@@ -15,6 +15,55 @@ public class Queen extends Figurine {
         boolean[][] possibleMoves = new boolean[8][8];
         // set all position as empty
         setAllPositionFalse(possibleMoves);
+        // set positive moves
+        int tmp_y = y + 1;
+        int tmp_x = x + 1;
+        while (tmp_x < possibleMoves.length && tmp_y < possibleMoves.length) {
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x++;
+            tmp_y++;
+        }
+        tmp_y = y - 1;
+        tmp_x = x + 1;
+        while (tmp_x < possibleMoves.length && tmp_y >= 0) {
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x++;
+            tmp_y--;
+        }
+        tmp_y = y - 1;
+        tmp_x = x - 1;
+        while (tmp_x >= 0 && tmp_y >= 0) {
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x--;
+            tmp_y--;
+        }
+        tmp_y = y + 1;
+        tmp_x = x - 1;
+        while (tmp_x >= 0 && tmp_y < possibleMoves.length) {
+            possibleMoves[tmp_y][tmp_x] = true;
+            tmp_x--;
+            tmp_y++;
+        }
+        tmp_y = y + 1;
+        while (tmp_y < possibleMoves.length) {
+            possibleMoves[tmp_y][x] = true;
+            tmp_y++;
+        }
+        tmp_y = y - 1;
+        while (tmp_y >= 0) {
+            possibleMoves[tmp_y][x] = true;
+            tmp_y--;
+        }
+        tmp_x = x + 1;
+        while (tmp_x < possibleMoves.length) {
+            possibleMoves[y][tmp_x] = true;
+            tmp_x++;
+        }
+        tmp_x = x - 1;
+        while (tmp_x >= 0) {
+            possibleMoves[y][tmp_x] = true;
+            tmp_x--;
+        }
         return possibleMoves;
     }
 }
