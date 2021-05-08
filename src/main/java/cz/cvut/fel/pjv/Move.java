9e72623de1858c7362d2f9076ac2aeaf1d8f279e
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-08 09:45:39 +0200

    generate setter and getter

--- src/main/java/cz/cvut/fel/pjv/Move.java
+++ src/main/java/cz/cvut/fel/pjv/Move.java
@@ -1,4 +1,48 @@
 package cz.cvut.fel.pjv;
 
+import cz.cvut.fel.pjv.Piece.Piece;
+import cz.cvut.fel.pjv.Player.Player;
+
 public class Move {
+    private Player player;
+    private Spot start;
+    private Spot end;
+    private Piece pieceMoved;
+    private Piece pieceKilled;
+    private boolean castlingMove = false;
+
+    public Move(Player player, Spot start, Spot end) {
+        this.player = player;
+        this.start = start;
+        this.end = end;
+        this.pieceMoved = start.getPiece();
+    }
+
+    public boolean isCastlingMove() {
+        return castlingMove;
+    }
+
+    public void setCastlingMove(boolean castlingMove) {
+        this.castlingMove = castlingMove;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Spot getStart() {
+        return start;
+    }
+
+    public Spot getEnd() {
+        return end;
+    }
+
+    public Piece getPieceMoved() {
+        return pieceMoved;
+    }
+
+    public Piece getPieceKilled() {
+        return pieceKilled;
+    }
 }
