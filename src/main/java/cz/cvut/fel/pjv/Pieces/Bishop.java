c5d560da6b2c9ff9bbc3a6c950a6048ccf578df5
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-08 16:31:10 +0200

    start working on isNotPiecesOnTheWay function

--- src/main/java/cz/cvut/fel/pjv/Pieces/Bishop.java
+++ src/main/java/cz/cvut/fel/pjv/Pieces/Bishop.java
@@ -11,7 +11,48 @@ public class Bishop extends Piece {
 
     @Override
     public boolean canMove(Board board, Spot start, Spot end) {
+
+        // protect spot with same color piece
+        if (isMyPieceInTheWay(end)) {
+            return false;
+        }
+
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+
+        if(x == y){
+            // check no piece between start and end position
+            if(isNotPiecesOnTheWay(board, start, end)){
+                return true;
+            }
+        }
+
         return false;
+    }
+
+    private boolean isNotPiecesOnTheWay(Board board, Spot start, Spot end){
+        // terminate arrow
+        int x = start.getX() - end.getX();
+        int y = start.getY() - end.getY();
+
+        // upper left
+        if(x>0 && y>0){
+
+        }
+        // down left
+        if(x<0 && y>0){
+
+        }
+        // up right
+        if(x>0 && y<0){
+
+        }
+        // down right
+        if(x<0 && y<0){
+
+        }
+
+        return true;
     }
 
     @Override
@@ -22,4 +63,5 @@ public class Bishop extends Piece {
             return WHITE + "♗";
         }
     }
+
 }
