f4b548b85e71f5835ab6023a8baa0efbbdb69bd0
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-08 09:12:04 +0200

    define class for players

---
+++ src/main/java/cz/cvut/fel/pjv/Player/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package cz.cvut.fel.pjv.Player;
+
+public class ComputerPlayer extends Player {
+
+    public ComputerPlayer(boolean whiteSide) {
+        this.whiteSide = whiteSide;
+        this.humanPlayer = false;
+    }
+}
---
+++ src/main/java/cz/cvut/fel/pjv/Player/HumanPlayer.java
@@ -0,0 +1,9 @@
+package cz.cvut.fel.pjv.Player;
+
+public class HumanPlayer extends Player {
+
+    public HumanPlayer(boolean whiteSide) {
+        this.whiteSide = whiteSide;
+        this.humanPlayer = true;
+    }
+}
