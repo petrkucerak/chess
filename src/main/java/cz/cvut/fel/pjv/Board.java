d5f0be250635785653281457a1329c0237f41b4e
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-08 08:37:39 +0200

    new file structure

--- src/main/java/cz/cvut/fel/pjv/Board.java
+++ src/main/java/cz/cvut/fel/pjv/Board.java
@@ -3,7 +3,7 @@ package cz.cvut.fel.pjv;
 /**
  * Class representations Chessboard as a 2D array.
  */
-public class Chessboard implements java.io.Serializable {
+public class Board implements java.io.Serializable {
 
 
 }
