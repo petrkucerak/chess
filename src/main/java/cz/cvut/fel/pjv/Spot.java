285c403148af1925048a2bfd29bb8040a6c98a51
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-08 08:49:19 +0200

    new object structure of Spot

--- src/main/java/cz/cvut/fel/pjv/Spot.java
+++ src/main/java/cz/cvut/fel/pjv/Spot.java
@@ -1,4 +1,42 @@
 package cz.cvut.fel.pjv;
 
+import cz.cvut.fel.pjv.Piece.Piece;
+
+/**
+ * Represent a cell on the chess board
+ */
 public class Spot {
+    private Piece piece;
+    private int x;
+    private int y;
+
+    public Spot(Piece piece, int x, int y) {
+        this.piece = piece;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
