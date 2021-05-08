034101d63ec77900bc9dd0d59bb886bda4485969
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-08 09:06:48 +0200

    define new object structure of Board

--- src/main/java/cz/cvut/fel/pjv/Board.java
+++ src/main/java/cz/cvut/fel/pjv/Board.java
@@ -3,7 +3,21 @@ package cz.cvut.fel.pjv;
 /**
  * Class representations Chessboard as a 2D array.
  */
-public class Board implements java.io.Serializable {
+public class Board {
+    Spot[][] boxes;
 
+    public Board() {
+        this.newBoard();
+    }
 
+    public Spot getBox(int x, int y) throws Exception {
+        if(x < 0 || x > 7 || y < 0 || y > 7){
+            throw new Exception("Index out of Chessboard!");
+        }
+        return this.boxes[x][y];
+    }
+
+    public void newBoard(){
+
+    }
 }
