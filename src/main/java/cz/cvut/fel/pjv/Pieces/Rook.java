e19cdf286c57020c12a9d98c6f22c3c7c56e971c
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-09 07:14:34 +0200

    fix function to checking the clear way for the rook

--- src/main/java/cz/cvut/fel/pjv/Pieces/Rook.java
+++ src/main/java/cz/cvut/fel/pjv/Pieces/Rook.java
@@ -10,7 +10,7 @@ public class Rook extends Piece {
     }
 
     @Override
-    public boolean canMove(Board board, Spot start, Spot end) {
+    public boolean canMove(Board board, Spot start, Spot end) throws Exception {
 
         // protect spot with same color piece
         if (isMyPieceInTheWay(end)) {
@@ -30,29 +30,58 @@ public class Rook extends Piece {
         return false;
     }
 
-    private boolean isNotPiecesOnTheWay(Board board, Spot start, Spot end) {
+    private boolean isNotPiecesOnTheWay(Board board, Spot start, Spot end) throws Exception {
         // terminate arrow
         int x = start.getX() - end.getX();
         int y = start.getY() - end.getY();
 
-        int i, j;
+        int i;
 
         // down
-        if (x > 0) {
-
+        if (x < 0) {
+            i = start.getX();
+            do {
+                i++;
+                if (i == end.getX()) {
+                    return true;
+                }
+            }
+            while (isOnTheBoard(i, start.getY()) && board.getBox(i, start.getY()).getPiece() == null);
         }
         // up
-        if (x < 0) {
-
+        if (x > 0) {
+            i = start.getX();
+            do {
+                i--;
+                if (i == end.getX()) {
+                    return true;
+                }
+            }
+            while (isOnTheBoard(i, start.getY()) && board.getBox(i, start.getY()).getPiece() == null);
         }
         // right
-        if (y > 0) {
-
+        if (y < 0) {
+            i = start.getY();
+            do {
+                i++;
+                if (i == end.getY()) {
+                    return true;
+                }
+            }
+            while (isOnTheBoard(start.getX(), i) && board.getBox(start.getX(), i).getPiece() == null);
         }
         // left
-        if (y < 0) {
-
+        if (y > 0) {
+            i = start.getY();
+            do {
+                i--;
+                if (i == end.getY()) {
+                    return true;
+                }
+            }
+            while (isOnTheBoard(start.getX(), i) && board.getBox(start.getX(), i).getPiece() == null);
         }
+        System.err.println("The way is not clear!");
         return false;
     }
 
