d6d335716f01fc391675941cb6123121105ece6c
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-09 07:43:37 +0200

    implement Queen validate methods

--- src/main/java/cz/cvut/fel/pjv/Pieces/Queen.java
+++ src/main/java/cz/cvut/fel/pjv/Pieces/Queen.java
@@ -10,7 +10,30 @@ public class Queen extends Piece {
     }
 
     @Override
-    public boolean canMove(Board board, Spot start, Spot end) {
+    public boolean canMove(Board board, Spot start, Spot end) throws Exception {
+
+        // protect spot with same color piece
+        if (isMyPieceInTheWay(end)){
+            return false;
+        }
+
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+
+        if(x == y){
+            // check no piece between start and end position
+            // use Rook validate method
+            if (isNotPiecesOnTheWayBishop(board, start, end)){
+                return true;
+            }
+        }
+        if((x == 0 && y > 0) || (x > 0 && y == 0)){
+            // check no piece between start and end position
+            // use Bishop validate method
+            if (isNotPiecesOnTheWayRook(board, start, end)) {
+                return true;
+            }
+        }
         return false;
     }
 
