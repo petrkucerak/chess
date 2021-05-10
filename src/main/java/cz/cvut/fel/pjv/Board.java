9c577ff0d74201ea661fcc1ee5e1a530b2ce96a6
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-10 15:43:00 +0200

    add board for testing castling

--- src/main/java/cz/cvut/fel/pjv/Board.java
+++ src/main/java/cz/cvut/fel/pjv/Board.java
@@ -15,7 +15,7 @@ public class Board {
      * For create new chessboard use newBoard function
      */
     public Board() {
-        this.testBoardPromotion();
+        this.testBoardCastling();
     }
 
     /**
@@ -85,6 +85,41 @@ public class Board {
                 }
             }
 
+        }
+
+    }
+
+    public void testBoardCastling() {
+        this.boxes = new Spot[8][8];
+        // place white pieces to spots
+        boxes[0][0] = new Spot(new Rook(false), 0, 0);
+        boxes[0][1] = new Spot(null, 0, 1);
+        boxes[0][2] = new Spot(null, 0, 2);
+        boxes[0][3] = new Spot(null, 0, 3);
+        boxes[0][4] = new Spot(new King(false), 0, 4);
+        boxes[0][5] = new Spot(null, 0, 5);
+        boxes[0][6] = new Spot(null, 0, 6);
+        boxes[0][7] = new Spot(new Rook(false), 0, 7);
+        for (int i = 0; i < boxes[1].length; i++) {
+            boxes[1][i] = new Spot(new Pawn(false), 1, i);
+        }
+        // place black pieces to spots
+        boxes[7][0] = new Spot(new Rook(true), 7, 0);
+        boxes[7][1] = new Spot(null, 7, 1);
+        boxes[7][2] = new Spot(null, 7, 2);
+        boxes[7][3] = new Spot(null, 7, 3);
+        boxes[7][4] = new Spot(new King(true), 7, 4);
+        boxes[7][5] = new Spot(null, 7, 5);
+        boxes[7][6] = new Spot(null, 7, 6);
+        boxes[7][7] = new Spot(new Rook(true), 7, 7);
+        for (int i = 0; i < boxes[1].length; i++) {
+            boxes[6][i] = new Spot(new Pawn(true), 6, i);
+        }
+        // place other spots as null
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < boxes[i].length; j++) {
+                boxes[i][j] = new Spot(null, i, j);
+            }
         }
 
     }
