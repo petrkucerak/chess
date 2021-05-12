585751491e8f240fd3198260a20d072a17ea683f
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-12 17:49:05 +0200

    add testing controller

--- src/main/java/cz/cvut/fel/pjv/ChessBoardController.java
+++ src/main/java/cz/cvut/fel/pjv/ChessBoardController.java
@@ -1,14 +1,15 @@
-package cz.cvut.fel.pjv.controller;
+package cz.cvut.fel.pjv;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class ChessBoardController {
 
-    /*@FXML
+    @FXML
     private void newGame() throws IOException {
-        System.out.println("NWE GAME");
-    }*/
+        System.out.println("NEW GAME");
+    }
 
 }
