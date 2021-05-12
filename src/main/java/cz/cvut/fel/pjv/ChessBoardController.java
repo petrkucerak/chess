c1ac5872e73405e7db3771ffce1564e68308e929
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-12 18:39:54 +0200

    implements generate chessboard by code

--- src/main/java/cz/cvut/fel/pjv/ChessBoardController.java
+++ src/main/java/cz/cvut/fel/pjv/ChessBoardController.java
@@ -2,16 +2,61 @@ package cz.cvut.fel.pjv;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 
 import java.io.IOException;
 
 public class ChessBoardController {
 
     @FXML
-    GridPane gridPane;
+    private void mouseEntered(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
+    }
+
+    @FXML
+    private GridPane grid ;
+
+    public void initialize() {
+        int numCols = 8 ;
+        int numRows = 8 ;
+
+        for (int i = 0 ; i < numCols ; i++) {
+            ColumnConstraints colConstraints = new ColumnConstraints();
+            colConstraints.setHgrow(Priority.SOMETIMES);
+            grid.getColumnConstraints().add(colConstraints);
+        }
+
+        for (int i = 0 ; i < numRows ; i++) {
+            RowConstraints rowConstraints = new RowConstraints();
+            rowConstraints.setVgrow(Priority.SOMETIMES);
+            grid.getRowConstraints().add(rowConstraints);
+        }
+
+        for (int i = 0 ; i < numCols ; i++) {
+            for (int j = 0; j < numRows; j++) {
+                addPane(i, j);
+            }
+        }
+    }
+
+    private void addPane(int colIndex, int rowIndex) {
+        Pane pane = new Pane();
+        if((colIndex%2 == 0 && rowIndex%2 == 0) || (colIndex%2 == 1 && rowIndex%2 == 1)){
+            pane.setStyle("-fx-background-color: #F3EACF;");
+        } else {
+            pane.setStyle("-fx-background-color: #774E24;");
+        }
+        pane.setOnMouseEntered(e -> {
+            System.out.printf("Mouse enetered cell [%d, %d]%n", colIndex, rowIndex);
+        });
+        grid.add(pane, colIndex, rowIndex);
+    }
 
     @FXML
     private void newGame() throws IOException {
@@ -19,17 +64,12 @@ public class ChessBoardController {
     }
 
     @FXML
-    private void getPosition() throws IOException {
-        System.out.println();
-    }
-
-    @FXML
-    private void saveGame(ActionEvent actionEvent) {
+    private void saveGame(ActionEvent actionEvent) throws IOException {
         System.out.println("SAVE GAME");
     }
 
     @FXML
-    private void loadGame(ActionEvent actionEvent) {
+    private void loadGame(ActionEvent actionEvent) throws IOException {
         System.out.println("LOAD GAME");
     }
 }
