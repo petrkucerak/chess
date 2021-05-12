d1c8f3d2fc7363436ce2324ec599b4fa4e77591b
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-12 15:37:54 +0200

    print basic style of chessboard

--- src/main/java/cz/cvut/fel/pjv/MainApp.java
+++ src/main/java/cz/cvut/fel/pjv/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("ChessBoard"), 640, 480);
+        scene = new Scene(loadFXML("ChessBoard"), 640, 800);
         stage.setScene(scene);
         stage.show();
     }
