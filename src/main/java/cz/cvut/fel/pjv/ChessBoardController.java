91eac4026ba37f679325cc7767eede4c7237fb2c
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-12 18:15:05 +0200

    add functions for all buttons

--- src/main/java/cz/cvut/fel/pjv/ChessBoardController.java
+++ src/main/java/cz/cvut/fel/pjv/ChessBoardController.java
@@ -1,15 +1,35 @@
 package cz.cvut.fel.pjv;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
 public class ChessBoardController {
 
     @FXML
+    GridPane gridPane;
+
+    @FXML
     private void newGame() throws IOException {
         System.out.println("NEW GAME");
     }
 
+    @FXML
+    private void getPosition() throws IOException {
+        System.out.println();
+    }
+
+    @FXML
+    private void saveGame(ActionEvent actionEvent) {
+        System.out.println("SAVE GAME");
+    }
+
+    @FXML
+    private void loadGame(ActionEvent actionEvent) {
+        System.out.println("LOAD GAME");
+    }
 }
