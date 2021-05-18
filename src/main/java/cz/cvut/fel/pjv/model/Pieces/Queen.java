3b7ec5a657c5a8eebc493610da76273970c55a45
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-18 23:05:51 +0200

    add JavaDocs

--- src/main/java/cz/cvut/fel/pjv/model/Pieces/Queen.java
+++ src/main/java/cz/cvut/fel/pjv/model/Pieces/Queen.java
@@ -3,31 +3,47 @@ package cz.cvut.fel.pjv.model.Pieces;
 import cz.cvut.fel.pjv.model.Board;
 import cz.cvut.fel.pjv.model.Spot;
 
+/**
+ * Class representation of the Queen piece.
+ */
 public class Queen extends Piece {
-
+    /**
+     * Create queen piece
+     *
+     * @param white
+     */
     public Queen(boolean white) {
         super(white);
     }
 
+    /**
+     * Methode to validate if the move is possible.
+     *
+     * @param board
+     * @param start
+     * @param end
+     * @return
+     * @throws Exception
+     */
     @Override
     public boolean canMove(Board board, Spot start, Spot end) throws Exception {
 
         // protect spot with same color piece
-        if (isMyPieceInTheWay(end)){
+        if (isMyPieceInTheWay(end)) {
             return false;
         }
 
         int x = Math.abs(start.getX() - end.getX());
         int y = Math.abs(start.getY() - end.getY());
 
-        if(x == y){
+        if (x == y) {
             // check no piece between start and end position
             // use Rook validate method
-            if (isNotPiecesOnTheWayBishop(board, start, end)){
+            if (isNotPiecesOnTheWayBishop(board, start, end)) {
                 return true;
             }
         }
-        if((x == 0 && y > 0) || (x > 0 && y == 0)){
+        if ((x == 0 && y > 0) || (x > 0 && y == 0)) {
             // check no piece between start and end position
             // use Bishop validate method
             if (isNotPiecesOnTheWayRook(board, start, end)) {
@@ -37,14 +53,24 @@ public class Queen extends Piece {
         return false;
     }
 
+    /**
+     * Method to get a symbol of the piece for print on board.
+     *
+     * @return
+     */
     @Override
     public String getPieceSymbol() {
         return "♛";
     }
 
+    /**
+     * Method to get a symbol of the piece for print on console board.
+     *
+     * @return
+     */
     @Override
     public String toString() {
-        if(this.isWhite()){
+        if (this.isWhite()) {
             return BLACK + "♕";
         } else {
             return WHITE + "♕";
