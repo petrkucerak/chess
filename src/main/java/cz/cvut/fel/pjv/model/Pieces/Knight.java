60fca2ed25775cf602ccb9d555251068c54e3d16
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-18 22:59:41 +0200

    add JavaDocs

--- src/main/java/cz/cvut/fel/pjv/model/Pieces/Knight.java
+++ src/main/java/cz/cvut/fel/pjv/model/Pieces/Knight.java
@@ -3,17 +3,33 @@ package cz.cvut.fel.pjv.model.Pieces;
 import cz.cvut.fel.pjv.model.Board;
 import cz.cvut.fel.pjv.model.Spot;
 
+/**
+ * Class representation of the Knight piece.
+ */
 public class Knight extends Piece {
 
+    /**
+     * Create knight piece.
+     *
+     * @param white
+     */
     public Knight(boolean white) {
         super(white);
     }
 
+    /**
+     * Methode to validate if the move is possible.
+     *
+     * @param board
+     * @param start
+     * @param end
+     * @return
+     */
     @Override
     public boolean canMove(Board board, Spot start, Spot end) {
 
         // protect spot with same color piece
-        if(isMyPieceInTheWay(end)){
+        if (isMyPieceInTheWay(end)) {
             return false;
         }
 
@@ -25,11 +41,21 @@ public class Knight extends Piece {
         return false;
     }
 
+    /**
+     * Method to get a symbol of the piece for print on board.
+     *
+     * @return
+     */
     @Override
     public String getPieceSymbol() {
         return "♞";
     }
 
+    /**
+     * Method to get a symbol of the piece for print on console board.
+     *
+     * @return
+     */
     @Override
     public String toString() {
         if (this.isWhite()) {
