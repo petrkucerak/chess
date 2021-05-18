9f07a18c23f2986a5aa6ef050672b5a65172422b
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-18 22:53:59 +0200

    add JavaDocs

--- src/main/java/cz/cvut/fel/pjv/model/Pieces/Bishop.java
+++ src/main/java/cz/cvut/fel/pjv/model/Pieces/Bishop.java
@@ -3,12 +3,29 @@ package cz.cvut.fel.pjv.model.Pieces;
 import cz.cvut.fel.pjv.model.Board;
 import cz.cvut.fel.pjv.model.Spot;
 
+/**
+ * Class representation of the bishop piece.
+ */
 public class Bishop extends Piece {
 
+    /**
+     * Create bishop piece
+     *
+     * @param white If is white, set to true.
+     */
     public Bishop(boolean white) {
         super(white);
     }
 
+    /**
+     * Methode to validate if the move is possible.
+     *
+     * @param board Game board.
+     * @param start
+     * @param end
+     * @return If move is possible, true.
+     * @throws Exception
+     */
     @Override
     public boolean canMove(Board board, Spot start, Spot end) throws Exception {
 
@@ -26,15 +43,24 @@ public class Bishop extends Piece {
                 return true;
             }
         }
-
         return false;
     }
 
+    /**
+     * Method to get a symbol of the piece for print on board.
+     *
+     * @return
+     */
     @Override
     public String getPieceSymbol() {
         return "♝";
     }
 
+    /**
+     * Method to get a symbol of the piece for print on console board.
+     *
+     * @return
+     */
     @Override
     public String toString() {
         if (this.isWhite()) {
