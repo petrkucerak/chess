72fb043f77705659770bfc0a421d692870c44cb6
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-19 10:46:51 +0200

    basic logic of timeclock

--- src/main/java/cz/cvut/fel/pjv/TimeClock/TheClock.java
+++ src/main/java/cz/cvut/fel/pjv/TimeClock/TheClock.java
@@ -1,4 +1,78 @@
 package cz.cvut.fel.pjv.TimeClock;
 
-public class TheClock {
+import static java.lang.Thread.sleep;
+
+public class TheClock implements Runnable {
+
+    private int timeLets;
+    Thread thr = null;
+    boolean threadSuspended;
+
+    public void setTimeLets(int timeLets) {
+        this.timeLets = timeLets;
+    }
+
+    public int getTimeLets() {
+        return timeLets;
+    }
+
+    public TheClock(int timeLets) {
+        this.timeLets = timeLets;
+    }
+
+    public void start() {
+        if (thr == null) {
+            thr = new Thread(this);
+            thr.setPriority(Thread.MIN_PRIORITY);
+            threadSuspended = false;
+            thr.start();
+        } else {
+            if (threadSuspended) {
+                threadSuspended = false;
+                synchronized (this) {
+                    notify();
+                }
+            }
+        }
+    }
+
+    public void stop() {
+        threadSuspended = true;
+    }
+
+    private void changeTime(int time) {
+        if (timeLets > 0) {
+            timeLets -= time;
+        } else {
+            System.err.println("Time < 0!");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("Time lefts: " + timeLets);
+                if (threadSuspended) {
+                    synchronized (this) {
+                        while (threadSuspended) {
+                            wait();
+                        }
+                    }
+                }
+                changeTime(1000);
+                thr.sleep(1000);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    public boolean isClockEnded() {
+        if (timeLets > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
