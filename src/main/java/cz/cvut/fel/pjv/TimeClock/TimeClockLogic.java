108a19a8217b0b293bc51dbe36b0619dea113c9a
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-19 10:46:33 +0200

    run basic clock

---
+++ src/main/java/cz/cvut/fel/pjv/TimeClock/TimeClockLogic.java
@@ -0,0 +1,18 @@
+package cz.cvut.fel.pjv.TimeClock;
+
+/**
+ * Implementations of custom Timeclock
+ */
+public class TimeClockLogic {
+    public static void main(String[]args) {
+        TheClock timer1 = new TheClock(10000);
+        TheClock timer2 = new TheClock(10000);
+        Thread thread1 = new Thread(timer1);
+        Thread thread2 = new Thread(timer2);
+        //thread1.setDaemon(true);
+        //thread2.setDaemon(true);
+
+        thread1.start();
+        thread2.start();
+    }
+}
