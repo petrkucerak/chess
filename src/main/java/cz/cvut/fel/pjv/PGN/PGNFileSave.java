00ae228d273dde1cf274fd1ed24ff9f8435832a4
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-25 23:51:50 +0200

    basic function for saving pgn data

--- src/main/java/cz/cvut/fel/pjv/PGN/PGNFileSave.java
+++ src/main/java/cz/cvut/fel/pjv/PGN/PGNFileSave.java
@@ -6,7 +6,29 @@ import java.io.*;
 
 public class PGNFileSave {
     public static void savePGNGame() {
+        String str = "";
+        str += MainApp.getGame().getPgnHeader();
+        str += "\n";
 
+        String line = "";
+        String[] words = MainApp.getGame().getPgnMoves().split(" ");
+        for (int i = 0; i < words.length; i++) {
+            line += words[i];
+            line += " ";
+        }
+        str += line;
+
+        try {
+            Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.pgn"), "UTF-8"));
+            out.write(str);
+            out.close();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
