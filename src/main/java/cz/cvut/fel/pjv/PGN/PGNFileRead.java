2947643aa57838eade1ca6b8bec0e9bb67dda09b
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-26 00:05:26 +0200

    create method definition

--- src/main/java/cz/cvut/fel/pjv/PGN/PGNFileRead.java
+++ src/main/java/cz/cvut/fel/pjv/PGN/PGNFileRead.java
@@ -1,4 +1,9 @@
 package cz.cvut.fel.pjv.PGN;
 
 public class PGNFileRead {
+    public static void readPGNFile(String pathname){
+        // read file
+        // load game from pgn
+        // set game to normal game
+    }
 }
