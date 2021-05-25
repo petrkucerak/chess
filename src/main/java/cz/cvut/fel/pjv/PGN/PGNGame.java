fdd8d59af847a37de4a51a4e85c45e9689984719
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-26 00:23:39 +0200

    add extends Game, generate basic getters and setters and init Game

--- src/main/java/cz/cvut/fel/pjv/PGN/PGNGame.java
+++ src/main/java/cz/cvut/fel/pjv/PGN/PGNGame.java
@@ -1,4 +1,34 @@
 package cz.cvut.fel.pjv.PGN;
 
-public class PGNGame {
+import cz.cvut.fel.pjv.model.Game;
+import cz.cvut.fel.pjv.model.Player.Player;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PGNGame extends Game {
+
+    private GameStatus finalStatus;
+    private int finalGameRound;
+
+    @Override
+    public void initGame(Player p1, Player p2) {
+        super.initGame(p1, p2);
+    }
+
+    public GameStatus getFinalStatus() {
+        return finalStatus;
+    }
+
+    public void setFinalStatus(GameStatus finalStatus) {
+        this.finalStatus = finalStatus;
+    }
+
+    public int getFinalGameRound() {
+        return finalGameRound;
+    }
+
+    public void setFinalGameRound(int finalGameRound) {
+        this.finalGameRound = finalGameRound;
+    }
 }
