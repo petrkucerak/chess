d6d695ca920786a12d445224ea251bb80dc68c99
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-26 08:38:52 +0200

    create variables for player names

--- src/main/java/cz/cvut/fel/pjv/model/Player/ComputerPlayer.java
+++ src/main/java/cz/cvut/fel/pjv/model/Player/ComputerPlayer.java
@@ -5,13 +5,24 @@ package cz.cvut.fel.pjv.model.Player;
  */
 public class ComputerPlayer extends Player {
 
+    private String name;
+
     public ComputerPlayer(boolean whiteSide) {
         this.whiteSide = whiteSide;
         this.humanPlayer = false;
+        this.name = "Computer, Player";
     }
 
     @Override
     public String toString() {
         return "Computer, Player";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/cz/cvut/fel/pjv/model/Player/HumanPlayer.java
+++ src/main/java/cz/cvut/fel/pjv/model/Player/HumanPlayer.java
@@ -5,13 +5,24 @@ package cz.cvut.fel.pjv.model.Player;
  */
 public class HumanPlayer extends Player {
 
+    private String name;
+
     public HumanPlayer(boolean whiteSide) {
         this.whiteSide = whiteSide;
         this.humanPlayer = true;
+        this.name = "Human, Player";
     }
 
     @Override
     public String toString() {
         return "Human, Player";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
