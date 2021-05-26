1f71d3d2f898ffa573666e12a7667a82c2d463a6
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-26 09:54:40 +0200

    add maven dependency for generate javadocs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
+    requires org.junit.jupiter.api;
 
     opens cz.cvut.fel.pjv to javafx.fxml;
     exports cz.cvut.fel.pjv;
