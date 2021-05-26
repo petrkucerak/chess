4c72b0ce20a136f08143f0cfaee0972000838760
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-26 15:09:31 +0200

    change permissions

--- src/main/java/cz/cvut/fel/pjv/Utils/Log.java
+++ src/main/java/cz/cvut/fel/pjv/Utils/Log.java
@@ -4,12 +4,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Log {
-    private static void turnLogOff() {
+    public static void turnLogOff() {
         Logger.getLogger("").setLevel(Level.OFF);
         Logger.getLogger("").getHandlers()[0].setLevel(Level.OFF);
     }
 
-    private static void turnLogOn() {
+    public static void turnLogOn() {
         Logger.getLogger("").setLevel(Level.INFO);
         Logger.getLogger("").getHandlers()[0].setLevel(Level.INFO);
     }
